954a9953d5792a115a7aa529f2db3bfeea759a08
Simon Smith <smithsps@gmail.com>
2014-02-02 17:27:27 -0500

    Ship Rotate vrrroooommm

--- src/Assets/MenuTHING.cs
+++ src/Assets/MenuTHING.cs
@@ -14,6 +14,10 @@ public class MenuTHING : MonoBehaviour {
 	public Rect ExitPos;
 	public GUIStyle buttonStyle;
 
+	public Texture ship;
+	public Rect shipR;
+	private int angle;
+
 	void OnGUI () {
 		GUI.DrawTexture(new Rect(0,0,1600,900), menuBG);
 		GUI.Label(titlePos, titleText ,titleStyle);
@@ -21,11 +25,16 @@ public class MenuTHING : MonoBehaviour {
 		if(GUI.Button(PlayPos, Play, buttonStyle)) {
 			Application.LoadLevel("Transition");
 		}
+
 		//So if not web player show close button.
 		if(!Application.isWebPlayer) {
 			if(GUI.Button(ExitPos, Exit, buttonStyle)) {
 				Application.Quit();
 			}
 		}
+		//Rotate that shi
+		GUIUtility.RotateAroundPivot(angle++/4, new Vector2(shipR.x, shipR.y));
+		GUI.DrawTexture(shipR, ship);
+
 	}
 }
