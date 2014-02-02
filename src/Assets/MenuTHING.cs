6d0d1af5352c9431ea25649c6bdee6da725e1f78
Simon Smith <smithsps@gmail.com>
2014-02-02 15:35:57 -0500

    MENU STUFF TEXT MUSIC FONTS THINGS MORE TTHINGS!

--- src/Assets/MenuTHING.cs
+++ src/Assets/MenuTHING.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class MenuTHING : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public Texture menuBG;
+	public string titleText;
+	public Rect titlePos;
+	public GUIStyle titleStyle;
+
+	public string Play;
+	public Rect PlayPos;
+	public string Exit;
+	public Rect ExitPos;
+	public GUIStyle buttonStyle;
+
+	void OnGUI () {
+		GUI.DrawTexture(new Rect(0,0,1600,900), menuBG);
+		GUI.Label(titlePos, titleText ,titleStyle);
+
+		GUI.Button(PlayPos, Play, buttonStyle);
+		GUI.Button(ExitPos, Exit, buttonStyle);
 	}
 }
