c96a73f3583721f81d1db3f66097b3e8870dc2c4
Simon Smith <smithsps@gmail.com>
2014-02-02 18:31:19 -0500

    MOAR

---
+++ src/Assets/ENDGUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ENDGUI : MonoBehaviour {
+
+	public Texture BG;
+	public Texture SHIP;
+
+	public GUIStyle style;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		//BG
+		GUI.DrawTexture(new Rect(0,0,1280,720),BG);
+		GUI.DrawTexture(new Rect(425,325,160,90),SHIP);
+
+		GUI.Label(new Rect(650, 650,500,100), "Credits: Simon, William, Ricky", style);
+	}
+}
